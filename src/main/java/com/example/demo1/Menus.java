604273ef78bb6385d879f1193c8e4b786ae56fb7
omidworor <127612765+omidworor@users.noreply.github.com>
2024-04-14 02:16:29 +0100

    add buttons to menu page

--- src/main/java/com/example/demo1/Menus.java
+++ src/main/java/com/example/demo1/Menus.java
@@ -1,4 +1,35 @@
 package com.example.demo1;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class Menus {
+
+    @FXML
+    private Button addNewMenu;
+
+    @FXML
+    private Button viewCurrentMenu;
+
+    @FXML
+    private Button viewPreviousMenus;
+
+
+    // Add action methods for other buttons as needed
+
+    private void showAlert(Alert.AlertType alertType, String title, String message, String details) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
