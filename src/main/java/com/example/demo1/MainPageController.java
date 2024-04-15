759b3eb10b3527cba80d2fcd0f4897ba5d1a524e
omidworor <127612765+omidworor@users.noreply.github.com>
2024-04-15 09:11:17 +0100

    fix main oage

--- src/main/java/com/example/demo1/MainPageController.java
+++ src/main/java/com/example/demo1/MainPageController.java
@@ -306,7 +306,7 @@ public class MainPageController {
         @FXML
         private void handleMenusButtonClick(ActionEvent event) {
             if (!"headchef".equals(currentUser) && !"souschef".equals(currentUser)){
-                Alert alert = new Alert (AlertType.ERROR);
+                Alert alert = new Alert (Alert.AlertType.ERROR);
                 alert.setTitle("Permission Denied");
                 alert.setHeaderText(null);
                 alert.setContentText("Not enough permissions to access this page.");
