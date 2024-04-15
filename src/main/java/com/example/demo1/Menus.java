2a3b09a13ab10f2bb214c9569568c2baf0ae307a
safaabdul <safa.abdul@city.ac.uk>
2024-04-15 02:10:33 +0100

    CurrentStock & CurrentStock.fxml

--- src/main/java/com/example/demo1/Menus.java
+++ src/main/java/com/example/demo1/Menus.java
@@ -138,12 +138,12 @@ public class Menus {
 
     @FXML
     private void handleSupplierButtonClick(ActionEvent event) {
-        navigateToPage("SupplierStock.fxml", "Stock", event);
+        navigateToPage("SupplierStock.fxml", "Supplier", event);
     }
 
     @FXML
     private void handleStockButtonClick(ActionEvent event) {
-        navigateToPage("Supplier.fxml", "Supplier", event);
+        navigateToPage("CurrentStock.fxml", "Stock", event);
     }
 
     @FXML
